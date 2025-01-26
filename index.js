c42a514df8c724fced078fb77f05b4cdae698a63
Thomas Carbonell <tcarbonell12@gmail.com>
2025-01-25 21:04:28 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const appendCat = [...cats, name];
+    return appendCat;
+}
+
+function prependCat(name) {
+    const prependCat = [name, ...cats];
+    return prependCat;
+}
+
+function removeLastCat() {
+    const removeLastCat = [...cats];
+    removeLastCat.pop();
+    return removeLastCat;
+}
+
+function removeFirstCat() {
+    const removeFirstCat = [...cats];
+    removeFirstCat.shift();
+    return removeFirstCat;
+}
